7fc7be232f9447b690437d735b2717007c952dea
Nikita Neganov <nikita@ridgebox.com>
2021-03-22 14:03:58 +0500

    Finish post CRUD

--- routes/posts.js
+++ routes/posts.js
@@ -20,14 +20,41 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/:id", async (req, res, next) => {
-  console.log(req.params, req.query);
-  // try {
-  //   const posts = await Post.find();
-  //   res.json(posts);
-  // } catch (error) {
-  //   res.json({ error });
-  // }
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.json(post);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    await Post.findByIdAndUpdate(
+      req.params.id,
+      {
+        ...req.body,
+      },
+      { useFindAndModify: false }
+    );
+
+    const updated = await Post.findOne({ _id: req.params.id });
+    res.json(updated);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await Post.findByIdAndRemove(req.params.id, {
+      useFindAndModify: false,
+    });
+    res.status(204);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 router.post("/", function (req, res, next) {
